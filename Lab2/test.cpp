8402b256cd553b735e479548bfcf044ed57d5c5e
Захар <104322009+Zaharazov@users.noreply.github.com>
2023-10-06 18:49:09 +0300

    Update test.cpp

--- Lab2/test.cpp
+++ Lab2/test.cpp
@@ -4,10 +4,10 @@
 TEST(test1, basic_test_set1)
 {
     Seven number1, number2;
-    ASSERT_TRUE(number1.to_string() == '0' && num2.to_string() == '0');
+    ASSERT_TRUE(number1.to_string() == "0" && number2.to_string() == "0");
 
-    number1 = "23";
-    number2 = "12";
+    number1 = {'2', '3'};
+    number2 = {'1', '2'};
     number2 = number2 + number1;
 
     EXPECT_FALSE(number1 == number2);
@@ -25,7 +25,7 @@ TEST(test2, basic_test_set2)
     number1 = number1 - number2; // 212 - 34 = 145
     EXPECT_TRUE(number1.to_string() == "145");
 
-    Seven number3 ('1', '0', '3');
+    Seven number3 {'1', '0', '3'};
     number3 = number3 + number2; // 103 + 34 = 140
     EXPECT_TRUE(number3.to_string() == "145");
 }
@@ -35,9 +35,9 @@ TEST(test3, basic_test_set3)
     Seven number1 {'1', '0', '0'};
     Seven number2 {'1'};
 
-    number1 = number1 - number2;
+    Seven number3 = number1 - number2;
     EXPECT_TRUE(number3.to_string() == "66");
 
-    number1 = "3";
+    number1 = {'3'};
     EXPECT_THROW(number2 - number1, std::underflow_error);
 }
