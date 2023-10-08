d597354f0e54ad59ea4645e09f41462f5aa2af08
Захар <104322009+Zaharazov@users.noreply.github.com>
2023-10-08 13:56:40 +0300

    Minor cosmetic changes in main.cpp

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -9,16 +9,14 @@ int main()
         char op;
         std::cout << "Enter an expression like: <value operator value> (separated by a space)" << std::endl;
         std::cin >> value1 >> op >> value2; 
- 
         Seven number1 = Seven(value1), number2 = Seven(value2);
- 
         switch (op)
         {
             case '+':
-                std::cout << (number1 + number2) << std::endl;
+                std::cout << (number1 + number2).to_string() << std::endl;
                 break;
             case '-':
-                std::cout << (number1 - number2) << std::endl;
+                std::cout << (number1 - number2).to_string() << std::endl;
                 break; 
             case '>':
                 std::cout << (number1 > number2) << std::endl;
@@ -30,11 +28,11 @@ int main()
                 std::cout << (number1 == number2) << std::endl;
                 break;
             default:
-                std::cout << "Incorrect expression." << std::endl;
+                std::cout << "Incorrect expression" << std::endl;
                 break;
         }
     }
-    catch (const std::invalid_argument& error)
+    catch (const std::invalid_argument& error) // what() - вывод ошибки в виде строки
     {
         std::cout << "Invalid argument: " << error.what() << std::endl;
     }
