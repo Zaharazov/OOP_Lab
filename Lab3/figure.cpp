888af11e9bf024076edff55084d7c69ef87838ce
Захар <104322009+Zaharazov@users.noreply.github.com>
2023-10-31 22:42:06 +0300

    Fixed "del" and "exit" issues

--- Lab3/figure.cpp
+++ Lab3/figure.cpp
@@ -7,8 +7,8 @@ bool Figure::operator == (const Figure& other) const
     return dots == other.dots;
 }
 
-Figure::~Figure() 
-{
-    dots.clear();
-    dots.shrink_to_fit();
-}
+// Figure::~Figure() 
+// {
+//     dots.clear();
+//     dots.shrink_to_fit();
+// }
