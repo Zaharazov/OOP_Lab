224c525cd67fe957f1e181f0a58266d8680e84ac
Захар <104322009+Zaharazov@users.noreply.github.com>
2023-12-17 02:19:23 +0300

    Create observer.hpp

---
+++ Lab6/observer.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+
+class NPC;
+
+class Observer 
+{
+    public:
+        Observer() = default;
+        virtual void update(NPC*, NPC*, bool) = 0;
+};
+
+class ConsoleObserver: public Observer 
+{
+    public:
+        ConsoleObserver() = default;
+        void update(NPC*, NPC*, bool) override;
+};
+
+class FileObserver: public Observer 
+{
+    std::unique_ptr<std::ostream> _stream;
+    public:
+        FileObserver(std::ofstream&);
+        void update(NPC*, NPC*, bool) override;
+};
