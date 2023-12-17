d9b7c9ff2d53b3180fa02f6e39b719c6653d0f7d
Захар <104322009+Zaharazov@users.noreply.github.com>
2023-12-17 04:26:50 +0300

    Create werewolf.hpp

---
+++ Lab7/werewolf.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "npc.hpp"
+
+struct Werewolf : public NPC
+{
+    Werewolf(int x, int y);
+    Werewolf(std::istream &is);
+
+    void print() override;
+
+    bool fight(std::shared_ptr<Werewolf> other) override;
+    bool fight(std::shared_ptr<Squirrel> other) override;
+    bool fight(std::shared_ptr<Druid> other) override;
+    bool accept(std::shared_ptr<NPC> visitor) override;
+
+    void save(std::ostream &os) override;
+
+    friend std::ostream &operator<<(std::ostream &os, Werewolf &Werewolf);
+};
